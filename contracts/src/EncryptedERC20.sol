f8727bc0bf18c4fcbf76b592a77e995b5add7f0a
Naman Mohnani <mohnaninaman@gmail.com>
2025-12-11 14:31:32 +0000

    feat: basic EncryptedERC20 setup

--- contracts/src/EncryptedERC20.sol
+++ contracts/src/EncryptedERC20.sol
@@ -1,8 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-// import "fhevm/lib/TFHE.sol";
+import {FHE} from "fhevm/lib/FHE.sol";
 
-import "fhevm/lib/FheType.sol";
+// Cannot inherit oz ERC20 contract because functions like transfer, approve, etc wont match the expected signatures
+contract EncryptedERC20 {
+    string private _name;
+    string private _symbol;
+    uint64 private _totalSupply;
+    uint8 public constant decimals = 6;
 
-contract EncryptedERC20 {}
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function totalSupply() public view returns (uint64) {
+        return _totalSupply;
+    }
+}
